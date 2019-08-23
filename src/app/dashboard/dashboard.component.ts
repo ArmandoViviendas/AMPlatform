e555590c4a7f50379020e1d5424258d3d278b5ee
Armando Reyes Perez <chif_arsenal95@hotmail.com>
2019-08-23 17:09:17 -0500

    cambios de juan y de Armando para el cambio de imagen de AM

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-container',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
 
-  constructor() { }
+export class DashboardComponent implements OnInit {
+  
+  constructor() { 
+    let inicio = "inicio";    
+
+  }
 
   ngOnInit() {
     console.log("dentro");
